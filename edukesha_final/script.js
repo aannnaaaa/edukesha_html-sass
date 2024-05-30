bbe77dd7915b7aa752cf11937679b6f042283444
aannnaaaa <146590524+aannnaaaa@users.noreply.github.com>
2024-05-30 21:54:09 +0500

    Add files via upload

---
+++ edukesha_final/script.js
@@ -0,0 +1,21 @@
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".links");
+let block = document.querySelector(".menu__block");
+let footer = document.querySelector(".menu__footer");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("btn__active");
+  menu.classList.toggle("links__active");
+  block.classList.toggle("menu__block__active");
+  footer.classList.toggle("menu__footer__active");
+});
+
+let myswiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  mobileFirst: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
